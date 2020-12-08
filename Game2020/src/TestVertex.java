b6a3091658468e96be57271f6680ee25cf7b8146
LucaEckenfels <LucaEckenfels@0-208-07>
2020-12-08 14:55:18 +0100

    Erster Hochladen

---
+++ Game2020/src/TestVertex.java
@@ -0,0 +1,8 @@
+
+public class TestVertex {
+	public static void main(String[] args) {
+		Vertex v1 = new Vertex(17, 4);
+		Vertex v2 = new Vertex(42, 0);
+		Vertex vv=new Vertex(0,0);
+	}
+}
---
+++ Game2020/src/Vertex.java
@@ -0,0 +1,10 @@
+public class Vertex {
+
+	public double x, y;
+
+	public Vertex(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
